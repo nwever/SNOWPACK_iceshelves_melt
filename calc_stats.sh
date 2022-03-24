90a7be2ffbd2348140f870e51937e29859db1e17
Nander Wever <nwever@gmail.com>
2022-03-24 11:36:20 -0600

    Removing code duplication by introducing an additional loop. Introducing switch for local time throughout.

--- calc_stats.sh
+++ calc_stats.sh
@@ -1,5 +1,6 @@
 iceshelves="$(grep -v ^# points_ice_shelves_4.45km.txt | awk -F, '{print $NF}' | sort -nu)"
 regions="$(seq 1 8)"
+lt=1	# Use local time (1), or UTC time (0)?
 
 #
 # Days with melt
@@ -11,21 +12,20 @@ do
 		echo "Processing region=${region}, th=${th}..."
 		outfile="./stats/meltdays_${th}mm_R${region}.txt"
 		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2.txt \
-		$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v melt_th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>melt_th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; s+=($9>0)?($9):(0); idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
+		$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v lt=${lt} -v melt_th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr((lt)?($3):($1),9,2); mm=substr((lt)?($3):($1),6,2); yr=substr((lt)?($3):($1),1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>melt_th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; s+=($9>0)?($9):(0); idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
 	done
 	for iceshelf in ${iceshelves}
 	do
 		echo "Processing iceshelf=${iceshelf}, th=${th}..."
 		outfile="./stats_iceshelves/meltdays_${th}mm_S${iceshelf}.txt"
 		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2.txt \
-		$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v melt_th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>melt_th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; s+=($9>0)?($9):(0); idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
+		$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v lt=${lt} -v melt_th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr((lt)?($3):($1),9,2); mm=substr((lt)?($3):($1),6,2); yr=substr((lt)?($3):($1),1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>melt_th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; s+=($9>0)?($9):(0); idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
 	done
 done
 
 #
 # Total melt
 #
-lt=1	# Use local time (1), or UTC time (0)?
 for region in ${regions}
 do
 	echo "Processing region=${region}..."
@@ -46,99 +46,31 @@ done
 #
 for th in 0 0.1 0.2 0.3 0.5 1 1.5 2
 do
-	# Total column liquid water in firn (from *.smet file)
-	for region in ${regions} 
+	for i in $(seq 5 10)
 	do
-		echo "Processing region=${region}, th=${th}..."
-		outfile="./stats/mswater_${th}mm_R${region}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
-		$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($5>s) {s=$5}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
-	done
-	for iceshelf in ${iceshelves}
-	do
-		echo "Processing iceshelf=${iceshelf}, th=${th}..."
-		outfile="./stats_iceshelves/mswater_${th}mm_S${iceshelf}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
-		$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($5>s) {s=$5}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
-	done
+		# Map columns to labels for output filenames
+		case "${i}" in
+			5) label="mswater" ;;		# Total column liquid water in firn (from *.smet file)
+			6 ) label="mswater0_0.1" ;;	# LWC 0-0.1m
+			7 ) label="mswater0_0.25" ;;	# LWC 0-0.25m
+			8 ) label="mswater0_0.5" ;;	# LWC 0-0.5m
+			9 ) label="mswater0_1" ;;	# LWC 0-1m
+			10 ) label="mswater0_2" ;;	# LWC 0-1m
+		esac
 
-	# LWC 0-0.1m
-	for region in ${regions} 
-	do
-		echo "Processing region=${region}, th=${th}..."
-		outfile="./stats/mswater0_0.1_${th}mm_R${region}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
-		$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($6>s) {s=$6}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
-	done
-	for iceshelf in ${iceshelves}
-	do
-		echo "Processing iceshelf=${iceshelf}, th=${th}..."
-		outfile="./stats_iceshelves/mswater0_0.1_${th}mm_S${iceshelf}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
-		$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($6>s) {s=$6}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
-	done
-
-	# LWC 0-0.25m
-	for region in ${regions} 
-	do
-		echo "Processing region=${region}, th=${th}..."
-		outfile="./stats/mswater0_0.1_${th}mm_R${region}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
-		$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($7>s) {s=$7}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
-	done
-	for iceshelf in ${iceshelves}
-	do
-		echo "Processing iceshelf=${iceshelf}, th=${th}..."
-		outfile="./stats_iceshelves/mswater0_0.1_${th}mm_S${iceshelf}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
-		$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($7>s) {s=$7}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
-	done
-
-	# LWC 0-0.5m
-	for region in ${regions}
-	do
-		echo "Processing region=${region}, th=${th}..."
-		outfile="./stats/mswater0_0.5_${th}mm_R${region}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
-		$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($8>s) {s=$8}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
-	done
-	for iceshelf in ${iceshelves}
-	do
-		echo "Processing iceshelf=${iceshelf}, th=${th}..."
-		outfile="./stats_iceshelves/mswater0_0.5_${th}mm_S${iceshelf}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
-		$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($8>s) {s=$8}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
-	done
-
-	# LWC 0-1m
-	for region in ${regions}
-	do
-		echo "Processing region=${region}, th=${th}..."
-		outfile="./stats/mswater0_1_${th}mm_R${region}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
-		$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($9>s) {s=$9}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
-	done
-	for iceshelf in ${iceshelves}
-	do
-		echo "Processing iceshelf=${iceshelf}, th=${th}..."
-		outfile="./stats_iceshelves/mswater0_1_${th}mm_S${iceshelf}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
-		$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($9>s) {s=$9}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
-	done
-
-	# LWC 0-2m
-	for region in ${regions}
-	do
-		echo "Processing region=${region}, th=${th}..."
-		outfile="./stats/mswater0_2_${th}mm_R${region}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
-		$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($10>s) {s=$10}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
-	done
-	for iceshelf in ${iceshelves}
-	do
-		echo "Processing iceshelf=${iceshelf}, th=${th}..."
-		outfile="./stats_iceshelves/mswater0_2_${th}mm_S${iceshelf}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
-		$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr($1,9,2); mm=substr($1,6,2); yr=substr($1,1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($10>s) {s=$10}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
+		for region in ${regions}
+		do
+			echo "Processing region=${region}, th=${th}..."
+			outfile="./stats/${label}_${th}mm_R${region}.txt"
+			bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
+			$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v col=${i} -v lt=${lt} -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr((lt)?($3):($1),9,2); mm=substr((lt)?($3):($1),6,2); yr=substr((lt)?($3):($1),1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($col>s) {s=$col}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
+		done
+		for iceshelf in ${iceshelves}
+		do
+			echo "Processing iceshelf=${iceshelf}, th=${th}..."
+			outfile="./stats_iceshelves/${label}_${th}mm_S${iceshelf}.txt"
+			bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
+			$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v col=${i} -v lt=${lt} -v th=${th} 'BEGIN {yr=-1; dd=-1} {if(substr($1,1,1)=="#") {if(yr!=-1) {i++}; yr=-1} else {dd=substr((lt)?($3):($1),9,2); mm=substr((lt)?($3):($1),6,2); yr=substr((lt)?($3):($1),1,4); if(mm>=11) {yr++}; years[yr]=yr; pp[i]=i; if(dd!=dd_old) {if(s>th && (mm>=11 || mm<=3)) {n[idx]++}; s=0}; if($col>s) {s=$col}; idx=sprintf("%d,%d", i, yr); dd_old=dd}} END {ny=asorti(years); ni=asorti(pp); for(y=1; y<=ny; y++) {printf("%d", years[y]); for(i=1; i<=ni; i++) {idx=sprintf("%d,%d", pp[i], years[y]); printf " %d", (idx in n)?(n[idx]):(0)}; printf "\n"}}' | Rscript calc_avg.R > ${outfile}
+		done
 	done
 done
