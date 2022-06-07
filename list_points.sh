5fd160d78194d8c3c853e6eb5ede41335bd34da1
Nander Wever <nwever@gmail.com>
2022-06-07 15:54:36 -0600

    Making the list comma separated for each column, and removing obsolete commented out lines of code

--- list_points.sh
+++ list_points.sh
@@ -9,14 +9,12 @@ for region in ${regions}
 do
 	bash pick_ok_points.sh postprocess/LATLON_MERRA2.zip *_MERRA2.txt \
 	$(grep -v ^# ../Spinup/points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') } | \
-	awk -v sel=${region} -F, '{print sel, $1 "," $2}'
-	#$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print sel, $1 "," $2}}')
+	awk -v sel=${region} -F, '{print sel "," $1 "," $2}'
 done
 echo "#Iceshelves"
 for iceshelf in ${iceshelves}
 do
 	bash pick_ok_points.sh postprocess/LATLON_MERRA2.zip *_MERRA2.txt \
 	$(grep -v ^# ../Spinup/points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | \
-	awk -v sel=${iceshelf} -F, '{print sel, $1 "," $2}'
-	#$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print sel, $1 "," $2}}')
+	awk -v sel=${iceshelf} -F, '{print sel "," $1 "," $2}'
 done
