5a3db7a2a6312adbcb3b0990a816eb503fb1f0f6
Nander Wever <nwever@gmail.com>
2022-08-17 15:29:50 -0600

    Making the "experiment" tag, with which the zip files are labelled, a variable setting, also in the calc_stats scripts.

--- calc_stats_per_day.sh
+++ calc_stats_per_day.sh
@@ -1,5 +1,6 @@
 iceshelves="$(grep -v ^# points_ice_shelves_4.45km.txt | awk -F, '{print $NF}' | sort -nu)"
 regions="$(seq 1 8)"
+experiment_tag="ICESHELVES"
 lt=1		# Use local time (1), or UTC time (0)?
 fullday=0	# Analyze 24 hr, 00:00-00:00 (1), or restrict analysis between 6:00-10:00 and 18:00-22:00 (0)?
 
@@ -29,14 +30,14 @@ do
 	do
 		echo "Processing region=${region}, th=${th}..."
 		outfile="./stats_per_day${sfx}/meltdays_${th}mm_R${region}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2.txt \
+		bash concatenate.sh postprocess/LATLON_${experiment_tag}.zip *_${experiment_tag}.txt \
 		$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v lt=${lt} -v melt_th=${th} -f days_with_melt_per_day.awk | Rscript calc_avg.R > ${outfile}
 	done
 	for iceshelf in ${iceshelves}
 	do
 		echo "Processing iceshelf=${iceshelf}, th=${th}..."
 		outfile="./stats_iceshelves_per_day${sfx}/meltdays_${th}mm_S${iceshelf}.txt"
-		bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2.txt \
+		bash concatenate.sh postprocess/LATLON_${experiment_tag}.zip *_${experiment_tag}.txt \
 		$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v lt=${lt} -v melt_th=${th} -f days_with_melt_per_day.awk | Rscript calc_avg.R > ${outfile}
 	done
 done
@@ -48,14 +49,14 @@ for region in ${regions}
 do
 	echo "Processing region=${region}..."
 	outfile="./stats_per_day${sfx}/melt_R${region}.txt"
-	bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2.txt \
+	bash concatenate.sh postprocess/LATLON_${experiment_tag}.zip *_${experiment_tag}.txt \
 	$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v lt=${lt} -f total_melt_per_day.awk | Rscript calc_avg.R > ${outfile}
 done
 for iceshelf in ${iceshelves}
 do
 	echo "Processing iceshelf=${iceshelf}..."
 	outfile="./stats_iceshelves_per_day${sfx}/melt_S${iceshelf}.txt"
-	bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2.txt \
+	bash concatenate.sh postprocess/LATLON_${experiment_tag}.zip *_${experiment_tag}.txt \
 	$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v lt=${lt} -f total_melt_per_day.awk | Rscript calc_avg.R > ${outfile}
 done
 
@@ -80,14 +81,14 @@ do
 		do
 			echo "Processing region=${region}, th=${th}..."
 			outfile="./stats_per_day${sfx}/${label}_${th}mm_R${region}.txt"
-			bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
+			bash concatenate.sh postprocess/LATLON_${experiment_tag}.zip *_${experiment_tag}_water.txt \
 			$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v col=${i} -v lt=${lt} -v fd=${fullday} -v th=${th} -f liquid_water_in_firn_per_day.awk | Rscript calc_avg.R > ${outfile}
 		done
 		for iceshelf in ${iceshelves}
 		do
 			echo "Processing iceshelf=${iceshelf}, th=${th}..."
 			outfile="./stats_iceshelves_per_day${sfx}/${label}_${th}mm_S${iceshelf}.txt"
-			bash concatenate.sh postprocess/LATLON_MERRA2.zip *_MERRA2_water.txt \
+			bash concatenate.sh postprocess/LATLON_${experiment_tag}.zip *_${experiment_tag}_water.txt \
 			$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v col=${i} -v lt=${lt} -v fd=${fullday} -v th=${th} -f liquid_water_in_firn_per_day.awk | Rscript calc_avg.R > ${outfile}
 		done
 	done
@@ -100,13 +101,13 @@ for region in ${regions}
 do
 	echo "Processing region=${region}..."
 	outfile="./stats_per_day${sfx}/fac_R${region}.txt"
-	bash concatenate.sh postprocess/LATLON_MERRA2.zip *_firn.txt \
+	bash concatenate.sh postprocess/LATLON_${experiment_tag}.zip *_firn.txt \
 	$(grep -v ^# points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v lt=${lt} -f fac_per_day.awk | Rscript calc_avg.R > ${outfile}
 done
 for iceshelf in ${iceshelves}
 do
 	echo "Processing iceshelf=${iceshelf}..."
 	outfile="./stats_iceshelves_per_day${sfx}/fac_S${iceshelf}.txt"
-	bash concatenate.sh postprocess/LATLON_MERRA2.zip *_firn.txt \
+	bash concatenate.sh postprocess/LATLON_${experiment_tag}.zip *_firn.txt \
 	$(grep -v ^# points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | awk -v lt=${lt} -f fac_per_day.awk | Rscript calc_avg.R > ${outfile}
 done
