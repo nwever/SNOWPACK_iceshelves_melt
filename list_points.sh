7c78335f7cd363711258e42a39ed6718a9acbe88
Nander Wever <nwever@gmail.com>
2023-05-22 05:56:23 -0600

    Need to point to the correct paths (latest simulations)

--- list_points.sh
+++ list_points.sh
@@ -7,14 +7,14 @@ regions="$(seq 1 8)"
 echo "#Regions"
 for region in ${regions}
 do
-	bash pick_ok_points.sh postprocess/LATLON_MERRA2.zip *_MERRA2.txt \
+	bash pick_ok_points.sh postprocess/LATLON_ICESHELVES.zip *_ICESHELVES.txt \
 	$(grep -v ^# ../Spinup/points_regions_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${region} -F, '{if($3==sel) {print $1 "," $2}}') } | \
 	awk -v sel=${region} -F, '{print sel "," $1 "," $2}'
 done
 echo "#Iceshelves"
 for iceshelf in ${iceshelves}
 do
-	bash pick_ok_points.sh postprocess/LATLON_MERRA2.zip *_MERRA2.txt \
+	bash pick_ok_points.sh postprocess/LATLON_ICESHELVES.zip *_ICESHELVES.txt \
 	$(grep -v ^# ../Spinup/points_ice_shelves_4.45km.txt | sed -e 's/\.88,/.875,/g' -e 's/\.38,/.375,/g' -e 's/\.12,/.125,/g' -e 's/\.62,/.625,/g' | awk -v sel=${iceshelf} -F, '{if($3==sel) {print $1 "," $2}}') | \
 	awk -v sel=${iceshelf} -F, '{print sel "," $1 "," $2}'
 done
